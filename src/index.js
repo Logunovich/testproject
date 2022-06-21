ecb0defd74b328209c01253a94060b1a58fd08cc
Denis <logunovich@gmail.com>
2022-06-21 17:24:44 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='/testproject'>
+    <BrowserRouter basename='testproject'>
         <App/>
     </BrowserRouter>
     );
